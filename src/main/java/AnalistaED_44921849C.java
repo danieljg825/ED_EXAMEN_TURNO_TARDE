62738a8b6cda59cd83625cd3c639528e2c0684e6
Daniel Jimenez Gutierrez 44921849C <danjimgut@outlook.es>
2022-01-17 15:59:55 +0100

    2º versión final con javadoc DNI 44921849C

--- src/main/java/AnalistaED_44921849C.java
+++ src/main/java/AnalistaED_44921849C.java
@@ -1,15 +1,33 @@
 import java.util.*;
 
+/**
+ *<h2><i>EJERCICIO PRÁCTICO EXAMEN EVAL 1 ED</i></h2>
+ * 
+ * @author DANIEL JIMENEZ GUTIERREZ DNI 44921849C
+ * @version 1.0
+ */
 public class AnalistaED_44921849C {
      static final double FACTOR_HORA_EXTRA = 1.2;
      private String nombreCompleto, dni;
      private double salarioBasePorHora = 10.56;
-     public AnalistaED_44921849C(String nombreCompleto, String dni) {
+     
+    /**
+     *Constructor con tres parámetros
+     * @param nombreCompleto nombre completo en mayúsculas
+     * @param dni documento nacional de identidad con letra y sin espacios
+     */
+    public AnalistaED_44921849C(String nombreCompleto, String dni) {
          this.nombreCompleto = nombreCompleto;
          this.dni = dni;
      }
      
-     public double getSalarioEsteMes(int horasExtra) {
+    /**
+     *Calcula el salario bruto mensual en función del salario base por hora y las horas extras.
+     *El precio por hora de la hora extra se determina con la constante FACTOR_HORA_EXTRA
+     * @param horasExtra número de horas extra dedicadas redondeado a la baja (entero)
+     * @return devuelve el salario bruto mensual en euros con dos decimales
+     */
+    public double getSalarioEsteMes(int horasExtra) {
         double cantidadExtra, salarioFinal;
         cantidadExtra = horasExtra * this.salarioBasePorHora *
         FACTOR_HORA_EXTRA;
